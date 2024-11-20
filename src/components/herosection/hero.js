c0c109e2680564dc31b5514b47d0c6f55c6cb306
shoaibalirts <shoaibali.rts@gmail.com>
2024-11-20 14:08:58 +0100

    applied dark and light mode

--- src/components/herosection/hero.js
+++ src/components/herosection/hero.js
@@ -19,21 +19,21 @@ export default function Hero({ heroData }) {
           {heroData.numberOfHomesOnSale} boliger til salg i{" "}
           {heroData.numberOfButikker} butikker
         </h3>
-        <form>
-          <p className={classes.form}>
-            <label htmlFor="search">Hvad skal din næste bolig indeholde</label>
-            <div className={classes.inputandsearchbtn}>
-              <input
-                className={classes.forminput}
-                id="search"
-                type="text"
-                placeholder="Søg på fx. glaskeramisk komfur, bryggers, kælder eller lignende"
-              />
-              <button onClick={handleSearch} className={classes.searchBtn}>
-                Søg
-              </button>
-            </div>
-          </p>
+        <form className={classes.form}>
+          {/* <p className={classes.form}> */}
+          <label htmlFor="search">Hvad skal din næste bolig indeholde</label>
+          <div className={classes.inputandsearchbtn}>
+            <input
+              className={classes.forminput}
+              id="search"
+              type="text"
+              placeholder="Søg på fx. glaskeramisk komfur, bryggers, kælder eller lignende"
+            />
+            <button onClick={handleSearch} className={classes.searchBtn}>
+              Søg
+            </button>
+          </div>
+          {/* </p> */}
         </form>
       </article>
     </section>
